96a4549f1b9a22a2ed12fe53d16eeeea8fb8bc26
praktikum22 <federico.falzon@gmx.ch>
2022-07-20 16:57:09 +0200

    Made stacked grouped barchat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
+import { TemplateLiteral } from '@angular/compiler';
 import { Component } from '@angular/core';
+
+import {ChartDataset,ChartOptions,ChartConfiguration} from "chart.js";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
+  
 })
 export class AppComponent {
-  title = 'barchart3';
+  public barChartData: any[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', stack: '1' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', stack: '1' },
+    { data: [42, 24, 71, 14, 31, 49, 30], label: 'Series C', stack: '2' },
+  ];
+  
+
+  public barChartLabels: string[] = [
+    '2006',
+    '2007',
+    '2008',
+    '2009',
+    '2010',
+    '2011',
+    '2012',
+  ];
+
+  public barChartOptions: any = {
+    responsive: true,
+  
+    scales: { 
+      xAxes: [
+        {
+          stacked: true,
+        },
+      ],
+      yAxes: [
+        {
+          stacked: true,
+        },
+      ],
+    },
+  };
+  public barChartType: string = 'bar';
+ 
+  
+  constructor() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgChartsModule } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
